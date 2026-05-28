8316a35396c81c43c74c62ff246acbd7c73ad3d3
Agung <agungananda17@icloud.com>
2026-05-29 02:10:31 +0800

    Welcome screen. Still need some reworks.

--- Challenge3/ContentView.swift
+++ Challenge3/ContentView.swift
@@ -9,13 +9,40 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            LinearGradient(
+                colors: [
+                    Color(red: 89/255, green: 193/255, blue: 253/255),
+                    Color(red: 63/255, green: 169/255, blue: 247/255)
+                ], startPoint: .top, endPoint: .bottom)
+            .ignoresSafeArea()
+            
+            ZStack {
+                Circle()
+                    .fill(Color.white .opacity(0.1))
+                    .frame(width: 700, height: 700)
+                Circle()
+                    .fill(Color.white .opacity(0.1))
+                    .frame(width: 380, height: 380)
+                Circle()
+                    .fill(Color.white .opacity(0.1))
+                    .frame(width: 260, height: 260)
+                
+            }
+            .offset(y:60)
+            
+            
+            VStack (alignment: .leading, spacing: 16){
+                Text("Manage \nYour Contents \nWith Lumio")
+                    .font(.system(size: 36, weight: .heavy))
+                    .foregroundStyle(Color.white)
+                    .lineSpacing(4)
+                Text("Create, collaborate, and organize \nyour contens in one space.")
+                    .font(.system(size: 15))
+                    .foregroundStyle(.white.opacity(0.85))
+            }
+            .offset(y:60)
         }
-        .padding()
     }
 }
 
