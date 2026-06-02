b42a7b4aa1322fec9aba0f180df27184a2dbcc39
Agung <agungananda17@icloud.com>
2026-06-02 15:21:51 +0800

    Screen for when user shares a photo/video from the gallery app to lumio

---
+++ Challenge3/Screens/ShareGalleryScreen.swift
@@ -0,0 +1,18 @@
+//
+//  ShareGalleryScreen.swift
+//  Challenge3
+//
+//  Created by Agung Ananda on 02/06/26.
+//
+
+import SwiftUI
+
+struct ShareGalleryScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShareGalleryScreen()
+}
