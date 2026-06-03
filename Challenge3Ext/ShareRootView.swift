a03e4fcf2f66909a0783518ce2bc8b6c8666f1d0
tankhang2004 <tankhang191104@gmail.com>
2026-06-03 09:40:37 +0800

    reorganize

--- Challenge3Ext/ShareRootView.swift
+++ Challenge3Ext/ShareRootView.swift
@@ -254,22 +254,4 @@ struct ShareRootView: View {
 //    }
 //}
 
-//import SwiftUI
-//
-//struct ShareRootView: View {
-//
-//    let onDone: () -> Void
-//
-//    var body: some View {
-//        VStack(spacing: 20) {
-//
-//            Text("Hello from Share Extension")
-//                .font(.title)
-//
-//            Button("Done") {
-//                onDone()
-//            }
-//        }
-//        .padding()
-//    }
-//}
+
